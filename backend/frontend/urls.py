72ead849bc2dcfab457ef3fbc42bdd7a0ca9bab5
coder <anuingale34@gmail.com>
2020-10-10 18:12:47 +0530

    all_code

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # path('', views.index),
-    path('add_city/<city>',views.add_city , name ='add_city'),
+    # path('add_city/<city>',views.add_city , name ='add_city'),
     path('delete/<city_name>/', views.delete_city, name='delete_city'),
 
 ]
--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -4,26 +4,25 @@ from .models import City
 from .forms import CityForm
 from django.http import HttpResponse
 from django.http import JsonResponse
-
-
-def add_city(request,city):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-    cities = City.objects.all()
-    weather_data = []
-    for city in cities:
-        r = requests.get(url.format(city)).json()
-        city_weather = {
-            'city': city.name,
-            'temperature': r["main"]["temp_min"],
-            'description': r["weather"][0]["description"],
-            'wind_flow': r["wind"]["speed"],
-            'icon': r["weather"][0]["icon"],
-        }
-        weather_data.append(city_weather)
-    context = {
-        'weather_data': weather_data,
-    }
-    return  JsonResponse(context)
+import  datetime
+# def add_city(request,city):
+#     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+#     cities = City.objects.all()
+#     weather_data = []
+#     for city in cities:
+#         r = requests.get(url.format(city)).json()
+#         city_weather = {
+#             'city': city.name,
+#             'temperature': r["main"]["temp_min"],
+#             'description': r["weather"][0]["description"],
+#             'wind_flow': r["wind"]["speed"],
+#             'icon': r["weather"][0]["icon"],
+#         }
+#         weather_data.append(city_weather)
+#     context = {
+#         'weather_data': weather_data,
+#     }
+#     return  JsonResponse(context)
 
 
 def home(request):
@@ -38,6 +37,9 @@ def home(request):
             existing_city_count = City.objects.filter(name=new_city).count()
             if existing_city_count == 0:
                 r = requests.get(url.format(new_city)).json()
+                date_name = r['dt']
+                date_m = datetime.datetime.fromtimestamp(date_name)
+                print(date_m.strftime('%Y-%m-%d'))
                 if r['cod'] == 200:
                     form.save()
                 else:
