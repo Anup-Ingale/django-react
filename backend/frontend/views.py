2f543c4d359366cff74347e4ebb7d220f2b1d1ec
coder <anuingale34@gmail.com>
2020-10-16 16:53:54 +0530

    all_code

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -5,6 +5,7 @@ from .forms import CityForm
 from django.http import HttpResponse
 from django.http import JsonResponse
 import  datetime
+
 # def add_city(request,city):
 #     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 #     cities = City.objects.all()
