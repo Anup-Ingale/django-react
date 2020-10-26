63a682044e895a16cfa281820625c2872fc780f8
coder <anuingale34@gmail.com>
2020-10-26 11:21:26 +0530

    all_code

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -7,7 +7,6 @@ from django.http import JsonResponse
 import  datetime
 
 
-
 def home(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     error_text = ''
